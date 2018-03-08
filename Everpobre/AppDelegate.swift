c678aa95b608cc43bea01b97935d6f37ab80c4d6
Joaquin Perez <jperbar@me.com>
2018-03-08 22:45:27 +0100

    End session

--- Everpobre/AppDelegate.swift
+++ Everpobre/AppDelegate.swift
@@ -16,6 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow()
+        
+        let noteVC = NoteViewController()
+        
+        window?.rootViewController = noteVC
+        
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
---
+++ Everpobre/NoteViewController.swift
@@ -0,0 +1,38 @@
+//
+//  NoteViewController.swift
+//  Everpobre
+//
+//  Created by Joaquin Perez on 08/03/2018.
+//  Copyright © 2018 Joaquin Perez. All rights reserved.
+//
+
+import UIKit
+
+class NoteViewController: UIViewController {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
