efe4dfa38de6b8c41a26e7c8b6c387dabf972b54
Joaquin Perez <jperbar@me.com>
2018-03-11 19:08:41 +0100

    Same implementation in Xib

--- Everpobre/AppDelegate.swift
+++ Everpobre/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         window = UIWindow()
         
-        let noteVC = NoteViewByCodeController() //NoteViewController()
+        let noteVC = NoteViewController()  // NoteViewByCodeController() 
         
         let navController = UINavigationController(rootViewController: noteVC)
         
